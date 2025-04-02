f892014b6493c26bcf22bedb338a310de41aef1f
KTomy7 <kisstomi7.kt@gmail.com>
2025-04-02 20:08:46 +0300

    Add stylesheet and logging to main application

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
+import logging
 from PyQt6.QtWidgets import QApplication
 from ui.main_window import MainWindow
+from utils import apply_stylesheet
 
 if __name__ == "__main__":
+    logging.info("CleanDesk started")
     app = QApplication(sys.argv)
+    apply_stylesheet(app)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
